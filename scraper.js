10043fd67de0bd1ad34cacbe36ee4eec8ec901cf
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-06-09 15:47:46 -0700

    Browser setup for web scraper

--- scraper.js
+++ scraper.js
@@ -1,3 +1,23 @@
 /**
  * Web scraper for books
  */
+
+ const puppeteer = require('puppeteer');
+ const { url } = require('./config');
+
+ ;(async () => {
+    //Open browser
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    //Setup browser
+    await page.setDefaultTimeout(10000);
+    await page.setViewport({ width: 1200, height: 800 });
+
+    //Close browser
+    await browser.close();
+
+ })().catch(error => {
+     console.log(error);
+     process.exit(1);
+ })

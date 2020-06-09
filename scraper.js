b1c863d4e6361b56d13c8f461a6968247b6e987b
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-06-09 15:58:02 -0700

    Data mining from the website

--- scraper.js
+++ scraper.js
@@ -8,13 +8,28 @@
 
  ;(async () => {
     //Open browser
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     //Setup browser
     await page.setDefaultTimeout(10000);
     await page.setViewport({ width: 1200, height: 800 });
     await page.setUserAgent(random_useragent.getRandom());
+
+    //Get data from bookstore
+    const name_selector = ".product_main > h1";
+    const price_selector = ".price_color";
+    await page.goto(url);
+    await page.waitForSelector(name_selector);
+    await page.waitForSelector(price_selector);
+
+    const name = await page.$eval(name_selector, element => element.innerHTML)
+    const price = await page.$eval(price_selector, element => element.innerHTML)
+    const nameTrim = name.trim();
+    const priceTrim = price.trim();
+
+    console.log("Name: " + nameTrim);
+    console.log("Price: " + priceTrim);
 
     //Close browser
     await browser.close();
